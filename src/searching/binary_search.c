614adf966d512974ff269ea167f0b43d56ea38b5
Karel Burda <karel.burda@nxp.com>
2018-12-31 20:18:03 +0100

    Implemented iterative version of binary search

--- src/searching/binary_search.c
+++ src/searching/binary_search.c
@@ -1,8 +1,20 @@
+#include <stdbool.h>
+
 #include "algorithms/searching/binary_search.h"
 
-static int binary_search_recursive(int array[], size_t left_index, size_t right_index, int element)
+static size_t get_middle_index(size_t left_index, size_t right_index)
 {
-    const size_t middle_index = (left_index + right_index) / 2;
+    return (left_index + right_index) / 2;
+}
+
+static int binary_search_recursive(int array[], size_t size, size_t left_index, size_t right_index, int element)
+{
+    if (left_index == right_index || right_index > size - 1)
+    {
+        return -1;
+    }
+
+    const size_t middle_index = get_middle_index(left_index, right_index);
     const int middle_element = array[middle_index];
 
     if (middle_element == element)
@@ -11,11 +23,37 @@ static int binary_search_recursive(int array[], size_t left_index, size_t right_
     }
     else if (middle_element > element)
     {
-        return binary_search_recursive(array, left_index, middle_index-1, element);
+        return binary_search_recursive(array, size, left_index, middle_index - 1, element);
     }
     else
     {
-        return binary_search_recursive(array, middle_index+1, right_index, element);
+        return binary_search_recursive(array, size, middle_index + 1, right_index, element);
+    }
+}
+
+static int binary_search_iterative(int array[], size_t size, size_t left_index, size_t right_index, int element) {
+    // TODO: optimize
+    for (size_t middle_index = get_middle_index(left_index, right_index); ; middle_index = get_middle_index(left_index, right_index))
+    {
+        if (left_index == right_index || right_index > size - 1)
+        {
+            return -1;
+        }
+
+        const int middle_element = array[middle_index];
+
+        if (middle_element == element)
+        {
+            return middle_index;
+        }
+        else if (middle_element > element)
+        {
+            right_index = middle_index - 1;
+        }
+        else
+        {
+            left_index = middle_element + 1;
+        }
     }
 }
 
@@ -23,7 +61,19 @@ int searching_binary_search_recursive(int array[], size_t size, int element)
 {
     if (array != NULL && size != 0)
     {
-        return binary_search_recursive(array, 0, size-1, element);
+        return binary_search_recursive(array, size, 0, size-1, element);
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int searching_binary_search_iterative(int array[], size_t size, int element)
+{
+    if (array != NULL && size != 0)
+    {
+        return binary_search_iterative(array, size, 0, size-1, element);
     }
     else
     {
--- tests/unit/src/searching/binary_search.cpp
+++ tests/unit/src/searching/binary_search.cpp
@@ -9,28 +9,41 @@ extern "C"
 
 namespace
 {
-TEST(binary_search_test_recursive, basic)
+TEST(binary_search_test, basic)
 {
     {
         int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
         EXPECT_EQ(searching_binary_search_recursive(array, 10, 5), 4);
+        EXPECT_EQ(searching_binary_search_iterative(array, 10, 5), 4);
+
+        EXPECT_EQ(searching_binary_search_recursive(array, 10, 999), -1);
+        EXPECT_EQ(searching_binary_search_iterative(array, 10, 999), -1);
+
+        EXPECT_EQ(searching_binary_search_recursive(array, 10, 0), -1);
+        EXPECT_EQ(searching_binary_search_iterative(array, 10, 0), -1);
     }
 
     {
         int array[] = { 1, 1, 1, 1, 1 };
+
         EXPECT_EQ(searching_binary_search_recursive(array, 5, 1), 2);
+        EXPECT_EQ(searching_binary_search_iterative(array, 5, 1), 2);
     }
 }
 
-TEST(binary_search_test_recursive, invalid)
+TEST(binary_search_test, invalid)
 {
     {
         int array[] = { 0 };
+
         EXPECT_EQ(searching_binary_search_recursive(array, 0, 0), -1);
+        EXPECT_EQ(searching_binary_search_iterative(array, 0, 0), -1);
     }
 
     {
         EXPECT_EQ(searching_binary_search_recursive(NULL, 0, 0), -1);
+        EXPECT_EQ(searching_binary_search_iterative(NULL, 0, 0), -1);
     }
 }
 }
