b32ddd9d337f61082a98859f7cb9629c8acb032b
Karel Burda <karel.burda@nxp.com>
2019-01-02 14:00:00 +0100

    Removed comments

--- include/algorithms/searching/binary_search.h
+++ include/algorithms/searching/binary_search.h
@@ -2,10 +2,6 @@
 
 #include <stddef.h>
 
-/// Time complexity: O (log n)
-/// Space complexity: O (n)
 int searching_binary_search_recursive(int array[], size_t size, int element);
 
-/// Time complexity: O (log n)
-/// Space complexity: O (1)
 int searching_binary_search_iterative(int array[], size_t size, int element);
