d3f4a0b32d5f1aeed0cfc6f668e8b6b3054cd8b1
Karel Burda <karel.burda@nxp.com>
2019-01-17 19:01:24 +0100

    Added qsort analysis

--- include/algorithms/sorting/quicksort.h
+++ include/algorithms/sorting/quicksort.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 /**
- * Time complexity -- TODO
- * Space complexity -- TODO
+ * Time complexity -- best and average case: O(n log n), worst case: O(n ^ 2)
+ * Space complexity -- all cases: O(log n)
  */
 int sorting_quicksort_recursive(int array[], size_t size);
