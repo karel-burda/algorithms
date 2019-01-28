58cb7ea9dfd8ddd894197dbb6ca616f563db83de
Unknown <karelburda89@gmail.com>
2019-01-28 09:19:34 +0100

    Using "%zd" to be compatible with 32-bit compilers

--- src/helper/array.c
+++ src/helper/array.c
@@ -6,7 +6,7 @@ void helper_print_array(int array[], size_t start_index, size_t end_index)
 {
     if (array != NULL)
     {
-        printf("Printing array located at %p (indexes %I64u-%I64u): ", array, start_index, end_index);
+        printf("Printing array located at %p (indexes %zd - %zd): ", (void *)array, start_index, end_index);
 
         for (size_t i = start_index; i <= end_index; ++i)
         {
