b5d5b04652e91d3d65ce66274c9ea75999965cb6
Karel Burda <karel.burda@nxp.com>
2019-02-03 15:23:24 +0100

    Added missing implementation file

---
+++ src/algorithms/bytes/endianness.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "algorithms/bytes/endianness.h"
+
+static endianness get_endianness()
+{
+    const u_int16_t number = 1;
+
+    if ((number & 0x1) == 1)
+    {
+        return E_LITTLE_ENDIAN;
+    }
+    else if (*((unsigned char *)&number) == 1)
+    {
+        return E_BIG_ENDIAN;
+    }
+    else
+    {
+        return E_UNKNOWN;
+    }
+}
+
+error_code bytes_get_endiannes(endianness * result)
+{
+    if (result != NULL)
+    {
+        *result = get_endianness();
+
+        return E_SUCCESS;
+    }
+    else
+    {
+        return E_INVALID_INPUT;
+    }
+}
