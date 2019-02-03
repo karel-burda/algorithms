13013c3aa7a9dbb16245e4fef68b36e97cd896d4
Karel Burda <karel.burda@nxp.com>
2019-02-03 15:18:47 +0100

    Improved bits test

--- tests/unit/src/algorithms/bits/bits_test.cpp
+++ tests/unit/src/algorithms/bits/bits_test.cpp
@@ -31,5 +31,25 @@ TEST(bits_get_number_of_one_bits, basic)
         bits_find_number_of_one_bits(512, &ones);
         EXPECT_EQ(ones, 1);
     }
+
+    {
+        bits_find_number_of_one_bits(0, &ones);
+        EXPECT_EQ(ones, 0);
+    }
+
+    {
+        bits_find_number_of_one_bits(-1, &ones);
+        EXPECT_EQ(ones, 16);
+    }
+
+    {
+        bits_find_number_of_one_bits(-3, &ones);
+        EXPECT_EQ(ones, 15);
+    }
+}
+
+TEST(bits_get_number_of_one_bits, invalid)
+{
+    EXPECT_EQ(bits_find_number_of_one_bits(2, NULL), E_INVALID_INPUT);
 }
 }
