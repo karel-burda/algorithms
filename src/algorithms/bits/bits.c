eaf6c8ee7d5a72a5b05a5f425d66ad6aa96f288e
Karel Burda <karel.burda@nxp.com>
2019-02-10 19:05:27 +0100

    Improved structure

--- src/algorithms/bits/bits.c
+++ src/algorithms/bits/bits.c
@@ -5,26 +5,31 @@
 
 static const unsigned char bits_in_byte = 8;
 
+static int find_number_of_one_bits(int number, unsigned char * one_bits)
+{
+    *one_bits = 0;
+    const unsigned char bit_shits = sizeof(number) * bits_in_byte;
+    unsigned char i = 0;
+
+    while (i < bit_shits)
+    {
+        if ((number & 0x1) == 1)
+        {
+            ++(*one_bits);
+        }
+
+        number >>= 1;
+        ++i;
+    }
+
+    return E_SUCCESS;
+}
+
 int bits_find_number_of_one_bits(int number, unsigned char * one_bits)
 {
     if (one_bits != NULL)
     {
-        *one_bits = 0;
-        const unsigned char bit_shits = sizeof(number) * bits_in_byte;
-        unsigned char i = 0;
-
-        while (i < bit_shits)
-        {
-            if ((number & 0x1) == 1)
-            {
-                ++(*one_bits);
-            }
-
-            number >>= 1;
-            ++i;
-        }
-
-        return E_SUCCESS;
+       return find_number_of_one_bits(number, one_bits);
     }
     else
     {
