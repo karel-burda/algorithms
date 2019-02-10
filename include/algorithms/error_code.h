bd00b06a9f037a0f70c2eafae1c8466f4cf67c5e
Karel Burda <karel.burda@nxp.com>
2019-02-10 18:57:59 +0100

    Explicit value

--- include/algorithms/error_code.h
+++ include/algorithms/error_code.h
@@ -5,7 +5,7 @@ enum _burda_algorithms_error_code
     E_SUCCESS = 0,
     E_INVALID_INPUT = -1,
     E_NOT_FOUND = -2,
-    E_FAILED_ALLOCATION
+    E_FAILED_ALLOCATION = -3
 };
 
 typedef enum _burda_algorithms_error_code error_code;
